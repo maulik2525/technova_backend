077aac42f7647cc1d358e5bc4c42bb5b7c7d7b33
Maulik Kamani <maulikkamani5@gmail.com>
2025-03-25 17:53:06 +0530

    small changes in middlware

--- src/middleware/multer.middleware.js
+++ src/middleware/multer.middleware.js
@@ -1,6 +1,6 @@
 import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
-import { cloudinary } from "../utils/cloudinary.js";
+import { cloudinary } from "../utils/Cloudinary.js";
 
 // Configure storage to handle both images and videos
 const storage = new CloudinaryStorage({
