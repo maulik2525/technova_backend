ffdf7abd7cf4b80d96b8614f373ebd6c98d1c10a
Maulik Kamani <maulikkamani5@gmail.com>
2025-03-26 12:02:22 +0530

    chang is port of frontend and backend

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import authRoutes from "./routes/auth.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
 import cookieParser from "cookie-parser";
-import path from "path";
 import { connectDB } from "./db/index.js";
 
 dotenv.config({
@@ -13,8 +12,6 @@ dotenv.config({
 });
 
 const port = process.env.PORT || 5000;
-const __dirname = path.resolve();
-
 const app = express();
 
 app.use(express.json({ limit: "16kb" }));
@@ -26,13 +23,6 @@ app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
-
-// Serve frontend from the correct directory
-app.use(express.static(path.join(__dirname, "..", "frontend-new", "dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "..", "frontend-new", "dist", "index.html"));
-});
 
 // Global Error Handler
 app.use((err, req, res, next) => {
